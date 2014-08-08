fd2ade2b1d858e4e6ee0846b6ffa12081eedd2d9
learntobot <3atapig@gmail.com>
2014-08-08 02:04:53 -0500

    Update Fletch.java

--- Fletch.java
+++ Fletch.java
@@ -4,10 +4,15 @@ import org.powerbot.script.Condition;
 import org.powerbot.script.rt6.GameObject;
 import org.powerbot.script.rt6.ClientContext;
 import org.powerbot.script.rt6.Item;
+import org.powerbot.script.rt6.Camera;
+import org.powerbot.script.rt6.Component;
+
 
 public class Fletch extends Task<ClientContext> {
   private int feather = 314;
   private int shaft = 52;
+  private int x = nextInt(5,85)
+  private int y = nextInt(0,360)
   
   public Fletch(ClientContext ctx) {
      super(ctx);
@@ -15,9 +20,11 @@ public class Fletch extends Task<ClientContext> {
   
   @Override
   public boolean activate() {
-    return ctx.backpack.inViewport()
-            && ctx.backpack().contains(feather + shaft)
-            && ctx.players.local().animation() == -1;
+    if{return ctx.backpack.inViewport();
+            && ctx.backpack().contains(feather + shaft);
+            && !component(42).inViewport();
+            && !component(20).inViewport();
+    } else  camera.pitch(x).yaw(y);
   }
   
   @Override
