83e9bb6bc78d395a0331211b32dbc936a8369288
learntobot <3atapig@gmail.com>
2014-08-08 00:47:15 -0500

    Update and rename Chop.java to Fletch.java

---
+++ Fletch.java
@@ -0,0 +1,29 @@
+package headless;
+
+import org.powerbot.script.Condition;
+import org.powerbot.script.rt6.GameObject;
+import org.powerbot.script.rt6.ClientContext;
+import org.powerbot.script.rt6.Item;
+
+public class Fletch extends Task<ClientContext> {
+  private int feather = 314;
+  private int shaft = 52;
+  
+  public Fletch(ClientContext ctx) {
+     super(ctx);
+  }
+  
+  @Override
+  public boolean activate() {
+    return ctx.backpack.inViewport()
+            && ctx.backpack().contains(feather + shaft)
+            && ctx.players.local().animation() == -1;
+  }
+  
+  @Override
+  public void execute() {
+    Condition.sleep();
+    feather.interact(true, shaft);
+    }
+  }
+}
